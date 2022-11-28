1275eed548b5961a342c823b12fcb75571208f22
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-11-29 00:34:01 +0600

    basicsetup

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const cors =require('cors');
 const app = express();
 const port =process.env.PORT || 5000;
 
+// middle wares
 app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('wild server is running')
+})
 
 
 
