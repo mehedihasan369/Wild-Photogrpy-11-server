35706cb7d407fd168dc724ba120dd69c1ef8609e
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-11-29 08:36:22 +0600

    services

--- index.js
+++ index.js
@@ -17,13 +17,26 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ewvrucy.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+  try{
+    const serviceCollection = client.db("wildPhoto").collection("services");
+    
+    app.get('/services',async (req,res) =>{
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    })
 
+  }
+  finally{
+
+  }
+
+}
+
+run().catch(err => console.error(err));
 
 app.get('/', (req, res) => {
   res.send('wild server is running')
