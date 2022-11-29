ea181428d19c7c12f974218e3dcd7450630dd0b4
mehedihasan369 <mehedihasan6472018@gmail.com>
2022-11-30 05:58:41 +0600

    post

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 console.log(process.env.DB_USER)
 console.log(process.env.DB_PASSWORD)
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ewvrucy.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,13 +21,43 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
   try{
     const serviceCollection = client.db("wildPhoto").collection("services");
+    const reviewCollection = client.db("wildPhoto").collection("reviews");
     
-    app.get('/services',async (req,res) =>{
+    app.get('/services-home',async (req,res) =>{
       const query = {}
       const cursor = serviceCollection.find(query);
       const services = await cursor.limit(3).toArray();
       res.send(services);
     })
+    app.get('/services',async (req,res) =>{
+      const query = {}
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+  });
+
+  ////reviews/--------------------------
+
+  app.get('/reviews', async (req, res) => {
+    let query = {};
+
+    if (req.query.email) {
+        query = {
+            email: req.query.email
+        }
+    }
+
+    const cursor = reviewCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+    
 
   }
   finally{
